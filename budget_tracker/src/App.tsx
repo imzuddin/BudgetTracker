ef730b5fb1622f674102eabfdd1d1f85fff75773
immyz-1 <imzuddin@gmail.com>
2023-09-06 12:17:30 -0400

    feat: settled on main design of homepage and menu bar

--- budget_tracker/src/App.tsx
+++ budget_tracker/src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 
 import { GenericCard } from './components/GenericCard';
+import Dropdown from './components/Dropdown';
 
 function App() {
   return (
     <div className="App">
         <div className='curve'>
+
+          <Dropdown/>
+
           <h1 className='title'> Welcome to Budget.io</h1>
           <h2 className='subtitle'> The one stop shop for budget planning </h2>
+
+
         </div>
     </div>
   );
