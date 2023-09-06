bebdf28f6dae42f3bdc9c3211a6843807f6d7e6d
immyz-1 <imzuddin@gmail.com>
2023-09-06 09:52:47 -0400

    feat: changed colors and layout, established general theme

--- budget_tracker/src/App.tsx
+++ budget_tracker/src/App.tsx
@@ -6,25 +6,10 @@ import { GenericCard } from './components/GenericCard';
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-
-        <section className='snap'>
-          <div className='curve layer'>
-            <h1 className='innerTitle'> Welcome, Here is your Dashboard</h1>
-          </div>
-
-          <div>
-            <GenericCard/>
-          </div>
-        </section>
-
-        <section className='snap'>
-          <div>
-            <GenericCard/>
-          </div>
-        </section>
-
-      </div>
+        <div className='curve'>
+          <h1 className='title'> Welcome to Budget.io</h1>
+          <h2 className='subtitle'> The one stop shop for budget planning </h2>
+        </div>
     </div>
   );
 }
